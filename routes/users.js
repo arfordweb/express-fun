f4e21f4359c89577b5276aa800a252dcad02231a
arfordweb <ugadarnell@gmail.com>
2019-02-18 08:20:09 -0500

    Add Postgres call to /users endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { Client } = require('pg')
+const client = new Client()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  await client.connect()
+
+  const pgres = await client.query(
+    'SELECT * FROM employees LEFT OUTER JOIN companies ON employees.company_id = companies.id;'
+  )
+  await client.end()
+  res.send(`<pre>${JSON.stringify(pgres.rows, null, 4)}</pre>`);
 });
 
 module.exports = router;
